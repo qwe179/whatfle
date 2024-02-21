545931e4b7cc00ce369d142149e2a98416dfada6
Pane <pane4846@gmail.com>
2024-02-22 00:15:21 +0900

    SwiftLint 추가 및 적용

--- What?fle/SceneDelegate.swift
+++ What?fle/SceneDelegate.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
                 window = UIWindow(windowScene: windowScene)
                 let viewController = ViewController()
@@ -29,4 +33,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {}
 }
-
