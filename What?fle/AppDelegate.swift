b218171975a5b76078392379a1aed28ef9092c0d
qwe179 <tkdwls0501@naver.com>
2024-07-10 11:00:38 +0900

    #13 [FEAT] PR, Issue 템플릿

--- What?fle/AppDelegate.swift
+++ What?fle/AppDelegate.swift
@@ -10,7 +10,6 @@ import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
